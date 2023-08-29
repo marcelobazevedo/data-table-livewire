7febc3f78a320a453b55969c14b7ec877588813b
Marcelo de Azevedo <marcelobazevedo@gmail.com>
2023-08-29 15:45:39 -0300

    MF

--- app/Livewire/UsersTable.php
+++ app/Livewire/UsersTable.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UsersTable extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+    public $perPage = 5;
+    public $admin = '';
     public function render()
     {
-        return view('livewire.users-table');
+        return view('livewire.users-table', [
+                'users' => User::search($this->search)
+                    ->when($this->admin !== '', function ($query){
+                        $query->where('is_admin', $this->admin);
+                    })
+                    ->paginate($this->perPage)
+            ]
+        );
     }
 }
